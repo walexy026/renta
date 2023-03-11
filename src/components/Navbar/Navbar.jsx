c384d4c69318e6c615a9beb08206d73a4750fc9a
walexy026 <jao.olaniyi@gmail.com>
2023-03-11 13:21:26 +0100

    started the nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import "./Navbar.scss";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="logo">RENTA</div>
+      <ul>
+        <li className="active">Insurance</li>
+        <li>Blog</li>
+        <li>About</li>
+        <li>
+          <button>Get Started</button>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
