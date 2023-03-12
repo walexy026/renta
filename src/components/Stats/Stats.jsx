0fcb44003eee55039ff0be7144e15d7cef5874a0
walexy026 <jao.olaniyi@gmail.com>
2023-03-12 09:10:49 +0100

    stats section

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -4,29 +4,29 @@ import hero from "../../Assets/heroImage.svg";
 const Stats = () => {
   return (
     <div className="stats">
-      <div>
+      <div className="statsText">
         <h2>We simply offer the best.</h2>
         <p>
           Settle in your dream home with Renta, we provide the best home options
           for families all around the country.
         </p>
         <div className="statsNumber">
-          <div>
+          <div className="statsFigure">
             <figure>500+</figure>
             <h6>Listed properties</h6>
           </div>
-          <div>
+          <div className="statsFigure">
             <figure>100+</figure>
             <h6>Happy customers</h6>
           </div>
-          <div>
+          <div className="statsFigure">
             <figure>35+</figure>
             <h6>Award winnings</h6>
           </div>
         </div>
       </div>
-      <div>
-        <img src={hero} alt="hero" />
+      <div className="statsImg">
+        <img src={hero} alt="hero" height="220px" />
       </div>
     </div>
   );
